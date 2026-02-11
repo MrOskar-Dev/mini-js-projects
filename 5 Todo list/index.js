92a04c0601a63e10aa9fe0479189c7c087211ea4
Oskar <oskarhawryleczko@gmail.com>
2026-02-11 15:58:51 +0000

    Simple todo list

---
+++ 5 Todo list/index.js
@@ -0,0 +1,56 @@
+let items = [];
+
+const itemsDiv = document.getElementById('items');
+const input = document.getElementById('itemInput');
+
+
+
+function renderItems () {
+  itemsDiv.innerHTML = null;
+
+  for (const [idx,item] of Object.entries(items)) {
+
+    const container = document.createElement('div');
+    container.style.marginBottom = '10px';
+
+    const text = document.createElement('p');
+    text.textContent = item;
+    text.style.display = 'inline';
+    text.style.marginRight = '10px'
+
+    const button = document.createElement('button');
+    button.textContent = 'Delete';
+    button.onclick = () => removeItem(idx);
+
+    container.appendChild(text);
+    container.appendChild(button);
+
+    itemsDiv.appendChild(container)
+  }
+}
+
+
+
+function loadItems () {}
+
+
+
+function saveItems () {}
+
+
+function addItem () {
+  const value = input.value;
+  if (!value) {
+    alert('You can not add an empty item')
+    return
+  }
+  items.push(value);
+  renderItems();
+  input.value = '';
+}
+
+
+function removeItem () {
+  items.splice(indexedDB, 1);
+  renderItems()
+}
